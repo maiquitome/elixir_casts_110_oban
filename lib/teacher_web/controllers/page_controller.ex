4732feb11d647a21b80ed01bdc245d4cc31b2dd8
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2022-03-14 21:36:22 -0300

    :zap: add create

--- lib/teacher_web/controllers/page_controller.ex
+++ lib/teacher_web/controllers/page_controller.ex
@@ -4,4 +4,19 @@ defmodule TeacherWeb.PageController do
   def index(conn, _params) do
     render(conn, "index.html")
   end
+
+  def create(conn, _params) do
+    # without oban:
+    # Teacher.Processor.process()
+
+    # with oban:
+    %{id: 1}
+    |> Teacher.Business.new()
+    |> Oban.insert()
+    # |> IO.inspect(label: "Finish Oban.insert()")
+
+    conn
+    |> put_flash(:info, "Work completed")
+    |> render("done.html")
+  end
 end
